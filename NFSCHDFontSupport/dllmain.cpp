c789165ac62360b034261f7c9deecba5ddaf4558
Aero_ <19331642+AeroWidescreen@users.noreply.github.com>
2019-08-24 14:17:32 -0700

    added support for FONT_NFS_MOVIE_LARGE

--- NFSCHDFontSupport/dllmain.cpp
+++ NFSCHDFontSupport/dllmain.cpp
@@ -9,25 +9,28 @@
 bool HDFontSupport;
 
 DWORD FontScaleCodeCave1Exit1 = 0x585D41;
-DWORD FontScaleCodeCave1Part2Exit = 0x585D28;
+DWORD FontScaleCodeCave1Exit2 = 0x585D28;
 DWORD FontScaleCodeCave2Exit1 = 0x71CA12;
-DWORD FontScaleCodeCave2Part2Exit = 0x71CA0E;
+DWORD FontScaleCodeCave2Exit2 = 0x71CA0E;
 
 void __declspec(naked) FontScaleCodeCave1()
 {
 	__asm {
+	FontScaleCodeCave1Check:
 		cmp dword ptr ds : [eax + 0x59], 0x544E4F46 // checks for "FONT"
-		jne FontScaleCodeCave1Part2
+		jne FontScaleCodeCave1None
 		cmp dword ptr ds : [eax + 0x5D], 0x53464E5F // checks for "_NFS_"
-		jne FontScaleCodeCave1Part2
-		cmp dword ptr ds : [eax + 0x61], 0x444F425F // checks for "BODY"
+		jne FontScaleCodeCave1None
+		cmp dword ptr ds : [eax + 0x61], 0x444F425F // checks for "BODY" (FONT_NFS_BODY)
 		je FontScaleCodeCave1Scale1
-		cmp dword ptr ds : [eax + 0x61], 0x5449545F // checks for "TITLE"
+		cmp dword ptr ds : [eax + 0x61], 0x5449545F // checks for "TITLE" (FONT_NFS_TITLE)
 		je FontScaleCodeCave1Scale2
-		jmp FontScaleCodeCave1Part2
+		cmp dword ptr ds : [eax + 0x61], 0x564F4D5F // checks for "MOVIE" (FONT_NFS_MOVIE_LARGE)
+		je FontScaleCodeCave1Scale3
+		jmp FontScaleCodeCave1None
 
 	FontScaleCodeCave1Scale1:
-		mov edi, 0x0100 // 256 FONT_NFS_BODY scale
+		mov edi, 0x0100 // 256: FONT_NFS_BODY
 		mov dword ptr ds : [esp + 0x0C], esi
 		mov dword ptr ds : [esp + 0x20], edi
 		dec esi
@@ -35,11 +38,11 @@ void __declspec(naked) FontScaleCodeCave1()
 		fild dword ptr ds : [esp + 0x20]
 		movzx esi, byte ptr ds : [edx + 0x02]
 		fstp dword ptr ds : [esp + 0x20]
-		mov eax, 0x0100 // 256 FONT_NFS_BODY scale
+		mov eax, 0x0100 // 256: FONT_NFS_BODY
 		jmp FontScaleCodeCave1Exit1
 
-	FontScaleCodeCave1Scale2 :
-		mov edi, 0x0200 // 512 FONT_NFS_TITLE scale
+	FontScaleCodeCave1Scale2:
+		mov edi, 0x0200 // 512: FONT_NFS_TITLE
 		mov dword ptr ds : [esp + 0x0C] , esi
 		mov dword ptr ds : [esp + 0x20] , edi
 		dec esi
@@ -47,45 +50,66 @@ void __declspec(naked) FontScaleCodeCave1()
 		fild dword ptr ds : [esp + 0x20]
 		movzx esi, byte ptr ds : [edx + 0x02]
 		fstp dword ptr ds : [esp + 0x20]
-		mov eax, 0x0100 // 256 FONT_NFS_TITLE scale
+		mov eax, 0x0100 // 256: FONT_NFS_TITLE
 		jmp FontScaleCodeCave1Exit1
 
-	FontScaleCodeCave1Part2:
+	FontScaleCodeCave1Scale3:
+		mov edi, 0x0200 // 512: FONT_NFS_MOVIE_LARGE
+		mov dword ptr ds : [esp + 0x0C] , esi
+		mov dword ptr ds : [esp + 0x20] , edi
+		dec esi
+		mov dword ptr ds : [esp + 0x08] , esi
+		fild dword ptr ds : [esp + 0x20]
+		movzx esi, byte ptr ds : [edx + 0x02]
+		fstp dword ptr ds : [esp + 0x20]
+		mov eax, 0x0200 // 512: FONT_NFS_MOVIE_LARGE
+		jmp FontScaleCodeCave1Exit1
+
+	FontScaleCodeCave1None:
 		movsx edi, word ptr ds : [eax + 0x28]
 		mov dword ptr ds : [esp + 0x0C], esi
-		jmp FontScaleCodeCave1Part2Exit
+		jmp FontScaleCodeCave1Exit2
 	}
 }
 
 void __declspec(naked) FontScaleCodeCave2()
 {
 	__asm {
-		cmp dword ptr ds : [ecx + 0x59] , 0x544E4F46 // checks for "FONT"
-		jne FontScaleCodeCave2Part2
-		cmp dword ptr ds : [ecx + 0x5D] , 0x53464E5F // checks for "_NFS_"
-		jne FontScaleCodeCave2Part2
-		cmp dword ptr ds : [ecx + 0x61] , 0x444F425F // checks for "BODY"
+	FontScaleCodeCave2Check:
+		cmp dword ptr ds : [ecx + 0x59], 0x544E4F46 // checks for "FONT"
+		jne FontScaleCodeCave2None
+		cmp dword ptr ds : [ecx + 0x5D], 0x53464E5F // checks for "_NFS_"
+		jne FontScaleCodeCave2None
+		cmp dword ptr ds : [ecx + 0x61], 0x444F425F // checks for "BODY" (FONT_NFS_BODY)
 		je FontScaleCodeCave2Scale1
-		cmp dword ptr ds : [ecx + 0x61] , 0x5449545F // checks for "TITLE"
+		cmp dword ptr ds : [ecx + 0x61], 0x5449545F // checks for "TITLE" (FONT_NFS_TITLE)
 		je FontScaleCodeCave2Scale2
-		jmp FontScaleCodeCave2Part2
+		cmp dword ptr ds : [ecx + 0x61], 0x564F4D5F // checks for "MOVIE" (FONT_NFS_MOVIE_LARGE)
+		je FontScaleCodeCave2Scale3
+		jmp FontScaleCodeCave2None
 
-	FontScaleCodeCave2Scale1 :
-		mov edx, 0x0100 // 256 FONT_NFS_BODY scale
+	FontScaleCodeCave2Scale1:
+		mov edx, 0x0100 // 256: FONT_NFS_BODY
 		mov dword ptr ds : [esp + 0x18] , edx
-		mov eax, 0x0100 // 256 FONT_NFS_BODY scale
+		mov eax, 0x0100 // 256: FONT_NFS_BODY
 		jmp FontScaleCodeCave2Exit1
 			
-	FontScaleCodeCave2Scale2 :
-		mov edx, 0x0200 // 512 FONT_NFS_TITLE scale
+	FontScaleCodeCave2Scale2:
+		mov edx, 0x0200 // 512: FONT_NFS_TITLE
 		mov dword ptr ds : [esp + 0x18] , edx
-		mov eax, 0x0100 // 256 FONT_NFS_TITLE scale
+		mov eax, 0x0100 // 256: FONT_NFS_TITLE
 		jmp FontScaleCodeCave2Exit1
 
-	FontScaleCodeCave2Part2 :
+	FontScaleCodeCave2Scale3:
+		mov edx, 0x0200 // 512: FONT_NFS_MOVIE_LARGE
+		mov dword ptr ds : [esp + 0x18] , edx
+		mov eax, 0x0200 // 512: FONT_NFS_MOVIE_LARGE
+		jmp FontScaleCodeCave2Exit1
+
+	FontScaleCodeCave2None:
 		movsx edx, word ptr ds : [ecx + 0x28]
 		mov dword ptr ds : [esp + 0x18] , edx
-		jmp FontScaleCodeCave2Part2Exit
+		jmp FontScaleCodeCave2Exit2
 	}
 }
 
--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # NFSC HD Font Support
 Support for high-resolution fonts in Need for Speed: Carbon. No textures included.  
-Use with NFS-TexEd v1.7 or newer.
 
 ## How To Use
 1. Move the *scripts* folder to the game's installation folder, where the executable is located.
 2. Download and extract [Ultimate-ASI-Loader.zip](https://github.com/ThirteenAG/Ultimate-ASI-Loader/releases).
 3. Move *dinput8.dll* to the game's installation folder, where the executable is located.
-4. Replace FONT_NFS_BODY and FONT_NFS_TITLE (GLOBALA.lzc) with your own high-resolution texture using NFS-TexEd v1.7.
+4. Replace the font with your own high-resolution texture using NFS-TexEd v1.7 or newer.
 
-## Note
-Please provide credits if you choose to bundle this with your texture mod.
+## Texture List
+- FONT_NFS_TITLE (GLOBALA.lzc)
+- FONT_NFS_BODY (GLOBALA.lzc)
+- FONT_NFS_MOVIE_LARGE (FRONTB1.lzc)
 
  ## Credits
 [Aero_](https://github.com/AeroWidescreen) : Creator of NFSC HD Font Support.  
